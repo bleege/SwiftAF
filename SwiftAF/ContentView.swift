ae6b8aff94e2c2fc4faf255e20368ee89ccb2f1f
Brad Leege <bleege@gmail.com>
2023-09-26 21:19:36 -0500

    Basic AFNetworking HTTP GET example

--- SwiftAF/ContentView.swift
+++ SwiftAF/ContentView.swift
@@ -5,15 +5,21 @@
 //  Created by Brad Leege on 9/26/23.
 //
 
+import AFNetworking
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject
+    private var viewModel = ConventViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Press To Get Data", action: {
+                viewModel.handleButtonPress()
+            })
+            .padding(.bottom, 20)
+            Text(viewModel.displayText)
         }
         .padding()
     }
@@ -21,4 +27,30 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+
+final class ConventViewModel: ObservableObject {
+    
+    @Published var displayText = "Hello, world!"
+    
+    func handleButtonPress() {
+        displayText = "button pressed"
+        
+        let url = URL(string: "https://jsonplaceholder.typicode.com")
+        
+        let afSessionManager = AFHTTPSessionManager(baseURL: url)
+        afSessionManager.get("comments/2",
+                             parameters: nil,
+                             headers: nil,
+                             progress: nil,
+                             success: { task, responseObject in
+            print("success responseObject = \(String(describing: responseObject))")
+        }, failure: { task, error in
+            print("error: \(error)")
+
+        })
+        
+    }
+    
 }
