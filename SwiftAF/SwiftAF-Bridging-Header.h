f42ea1ad61c2fe86c8599af986fe22c8488cbab1
Brad Leege <bleege@gmail.com>
2023-09-26 20:16:59 -0500

    Importing AFNetworking along with Bridging Header

---
+++ SwiftAF/SwiftAF-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SwiftAF-Bridging-Header.h
+//  SwiftAF
+//
+//  Created by Brad Leege on 9/26/23.
+//
+
+#ifndef SwiftAF_Bridging_Header_h
+#define SwiftAF_Bridging_Header_h
+
+@import AFNetworking;
+
+#endif /* SwiftAF_Bridging_Header_h */
