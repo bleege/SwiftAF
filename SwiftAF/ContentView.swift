12822624639dbd72e4f947d7f5a5c10b59ca7be6
Brad Leege <bleege@gmail.com>
2023-09-26 19:46:37 -0500

    Initial Commit

---
+++ SwiftAF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftAF
+//
+//  Created by Brad Leege on 9/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftAF/SwiftAFApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAFApp.swift
+//  SwiftAF
+//
+//  Created by Brad Leege on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
